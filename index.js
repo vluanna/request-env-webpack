f1e373df14f7440a3eac6844bd3089816b65bf7a
Luân Nguyễn <55823278+vluanna@users.noreply.github.com>
2021-05-24 21:37:31 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,47 @@
+const axios = require("axios");
+const webpack = require("webpack");
+// A JavaScript class.
+module.exports = class RequestEnvWebpack {
+    static defaultOptions = {
+        url: null,
+        method: 'GET'
+    };
+    constructor(options = {}) {
+        this.options = { ...RequestEnvWebpack.defaultOptions, ...options };
+    }
+    apply(compiler) {
+        compiler.hooks.beforeRun.tapAsync(
+            "RequestEnvWebpack",
+            async (compilation, callback) => {
+                const setEnv = (env) => {
+                    const envObj = {};
+                    Object.keys(env || {}).forEach((key) => {
+                        envObj[key] = env[key];
+                    });
+                    new webpack.DefinePlugin({
+                        "process.env": JSON.stringify(envObj),
+                    }).apply(compiler);
+                };
+                if (!this.options.url) {
+                    setEnv(require("dotenv").config().parsed);
+                } else {
+                    try {
+                        const configResp = await axios(this.options);
+                        if (configResp.data.isSuccess) {
+                            const envObj = {};
+                            (configResp.data.data || []).forEach(item => {
+                                envObj[item.key] = item.value;
+                            })
+                            setEnv(envObj);
+                        } else {
+                            throw new Error("Cannot get config from url:" + JSON.stringify(configResp.data.errors));
+                        }
+                    } catch (error) {
+                        throw new Error("Cannot get config from url:" + String(error));
+                    }
+                }
+                callback();
+            }
+        );
+    }
+};
