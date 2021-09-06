0b2ed5b841da2fe517982091286cb049dd96fb0f
Luân Nguyễn <55823278+vluanna@users.noreply.github.com>
2021-09-07 00:35:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const webpack = require("webpack");
 const dotEnv = require("dotenv");
+const fs = require('fs');
 
 const setEnv = (env, compiler) => {
     const envObj = {};
@@ -8,25 +9,45 @@ const setEnv = (env, compiler) => {
         envObj[key] = env[key];
     });
     process.env = Object.assign(process.env, envObj)
+    const envStringData = JSON.stringify(envObj)
     new webpack.DefinePlugin({
-        "process.env": JSON.stringify(envObj),
+        "process.env": envStringData,
     }).apply(compiler);
 };
+
+const configParser = (config) => config && config.data;
 
 const pluginName = "RequestEnvWebpack";
 
 module.exports = class RequestEnvWebpack {
-    
+
     static defaultOptions = {
         url: null,
-        method: 'GET'
+        method: 'GET',
+        configPath: process.env.CONFIG_PATH,
+        configParser,
     };
 
     constructor(options = {}) {
         this.options = { ...RequestEnvWebpack.defaultOptions, ...options };
     }
     apply(compiler) {
-        if (!this.options.url) {
+        if (this.options.configPath) {
+            compiler.hooks.beforeRun.tapAsync(
+                pluginName,
+                async (compilation, callback) => {
+                    const configPath = this.options.configPath || process.env.CONFIG_PATH;
+                    try {
+                        const rawConfig = fs.readFileSync(configPath);
+                        const config = JSON.parse(rawConfig);
+                        setEnv(this.options.configParser(config), compiler)
+                    } catch (error) {
+                        throw new Error(pluginName + ": Cannot get config from config path:" + configPath + 'Error: ' + JSON.stringify(error));
+                    }
+                    callback();
+                }
+            );
+        } else if (!this.options.url) {
             compiler.hooks.environment.tap(
                 pluginName,
                 (compilation) => {
@@ -44,12 +65,12 @@ module.exports = class RequestEnvWebpack {
                             (configResp.data.data || []).forEach(item => {
                                 envObj[item.key] = item.value;
                             })
-                            setEnv(envObj, compiler);
+                            setEnv(this.options.configParser({ data: envObj }), compiler);
                         } else {
-                            throw new Error("Cannot get config from url:" + JSON.stringify(configResp.data.errors));
+                            throw new Error(pluginName + ": Cannot get config from url:" + JSON.stringify(configResp.data.errors));
                         }
                     } catch (error) {
-                        throw new Error("Cannot get config from url:" + String(error));
+                        throw new Error(pluginName + ": Cannot get config from url:" + String(error));
                     }
                     callback();
                 }
